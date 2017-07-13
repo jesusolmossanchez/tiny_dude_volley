03cc874b9502138362173df24e96dd92553e8697
jesusolmossanchez <jesusolmossanchez@gmail.com>
2017-07-13 12:21:26 +0200

    Ball preparado para optimización

--- Ball.js
+++ Ball.js
@@ -1,27 +1,27 @@
 
 
 /**************************************************
-** GAME PLAYER CLASS
+** GAME BALL CLASS
 **************************************************/
 var Ball = function(juego, x, y, gravedad, impulso) {
 
     this.x                = 96;
     this.y                = 0;
-    this.alto             = 50;
-    this.ancho            = 50;
-    this.center_x         = this.x + this.ancho/2;
-    this.center_y         = this.y + this.alto/2;
+    this.alto_             = 50;
+    this.ancho_            = 50;
+    this.center_x_         = this.x + this.ancho_/2;
+    this.center_y_         = this.y + this.alto_/2;
     this.dx               = 0;
     this.dy               = 0;
-    this.gravity          = 900;
-    this.maxdx            = 1500;
-    this.maxdy            = 1500;
-    this.impulse          = 550;
-    this.accel            = this.maxdx / (juego.ACCEL);
-    this.friction         = this.maxdx / (juego.FRICTION);
-    this.ball             = true;
-    this.start            = { x: this.x, y: this.y };
-    this.theta = 0;
+    this.gravity_          = 900;
+    this.maxdx_            = 1500;
+    this.maxdy_            = 1500;
+    this.impulse_          = 550;
+    this.accel_            = this.maxdx_ / (juego.ACCEL_);
+    this.friction_         = this.maxdx_ / (juego.FRICTION_);
+    this.ball_             = true;
+    this.start_            = { x: this.x, y: this.y };
+    this.theta_ = 0;
 
 
 
@@ -31,7 +31,7 @@ var Ball = function(juego, x, y, gravedad, impulso) {
 
         //reseteo las aceleraciones
         this.ddx = 0;
-        this.ddy = this.gravity;
+        this.ddy = this.gravity_;
   
         //Posicion
         this.x  = this.x  + (dt * this.dx);
@@ -39,52 +39,52 @@ var Ball = function(juego, x, y, gravedad, impulso) {
 
         //Velocidad
         //TODO: Revisar velocidad de la pelota
-        this.dy = juego.bound(this.dy + (dt * this.ddy), -this.maxdy, this.maxdy);
+        this.dy = juego.bound_(this.dy + (dt * this.ddy), -this.maxdy_, this.maxdy_);
         this.dx = this.dx/1.0004;
 
 
         //pelota cayendo...
         if (this.dy > 0) {
-            if((this.y + this.alto) > juego.alto_total){
-                this.dy = -this.dy * juego.FACTOR_REBOTE;
+            if((this.y + this.alto_) > juego.alto_total_){
+                this.dy = -this.dy * juego.FACTOR_REBOTE_;
 
                 //TOCA el suelo, procesa punto
-                if(!juego.hay_punto){
+                if(!juego.hay_punto_){
      
                     window.punto_audio.play();
 
-                    var x_explosion = this.center_x;
-                    var y_explosion = this.center_y + this.alto;
+                    var x_explosion = this.center_x_;
+                    var y_explosion = this.center_y_ + this.alto_;
                     
-                    juego.explosions.push(
+                    juego.explosions_.push(
                         new Explosion(x_explosion, y_explosion, true, true, this)
                     );
 
-                    juego.tiempo_punto = juego.timestamp() + 3000;
+                    juego.tiempo_punto_ = juego.timestamp_() + 4000;
                     
-                    if(this.center_x < juego.ancho_total/2){
+                    if(this.center_x_ < juego.ancho_total_/2){
                         juego.ultimo_rebote = 2;
-                        if(juego.puntos2 >= 9){
-                            juego.ganador = 2;
-                            juego.game_over(juego.ctx);
+                        if(juego.puntos2_ >= 9){
+                            juego.ganador_ = 2;
+                            juego.game_over_(juego.ctx);
                         }
                         else{
-                            juego.puntos2++;
-                            juego.empieza1 = false;
+                            juego.puntos2_++;
+                            juego.empieza1_ = false;
                             
                         }
                     }
                     else{
-                        if(juego.puntos1 >= 9){
-                            juego.ganador = 1;
-                            juego.game_over(juego.ctx);
+                        if(juego.puntos1_ >= 9){
+                            juego.ganador_ = 1;
+                            juego.game_over_(juego.ctx);
                         }
-                        if(juego.puntos1 >= 4 && juego.level < 9){
-                            juego.siguiente_level();
+                        if(juego.puntos1_ >= 4 && juego.level_ < 9){
+                            juego.siguiente_level_();
                         }
                         else{
-                            juego.puntos1++;
-                            juego.empieza1 = true;
+                            juego.puntos1_++;
+                            juego.empieza1_ = true;
                         }
 
                     }
@@ -95,21 +95,21 @@ var Ball = function(juego, x, y, gravedad, impulso) {
         //pelota subiendo
         else if (this.dy < 0) {
             if(this.y <= 0){
-                this.dy  = - this.dy * juego.FACTOR_REBOTE;
+                this.dy  = - this.dy * juego.FACTOR_REBOTE_;
             }
         }
   
         //pelota va a la derecha
         if (this.dx > 0) {
-            if((this.center_x + this.ancho/2) > juego.ancho_total){
-                this.dx = -this.dx * juego.FACTOR_REBOTE;
+            if((this.center_x_ + this.ancho_/2) > juego.ancho_total_){
+                this.dx = -this.dx * juego.FACTOR_REBOTE_;
             }
         }
         //pelota va a la izquierda
         else if (this.dx < 0) {
 
-            if((this.center_x - this.ancho/2) <= 0){
-                this.dx = -this.dx * juego.FACTOR_REBOTE;
+            if((this.center_x_ - this.ancho_/2) <= 0){
+                this.dx = -this.dx * juego.FACTOR_REBOTE_;
 
             }
         }
@@ -118,10 +118,10 @@ var Ball = function(juego, x, y, gravedad, impulso) {
 
 
 
-        if(juego.hay_punto){
-            this.dy = juego.bound(this.dy/1.05, -500, 500);
-            this.dx = juego.bound(this.dx/1.05, -600, 600);
-            //this.ddy = juego.bound(this.ddy/1.05, -100, 100);
+        if(juego.hay_punto_){
+            this.dy = juego.bound_(this.dy/1.05, -500, 500);
+            this.dx = juego.bound_(this.dx/1.05, -600, 600);
+            //this.ddy = juego.bound_(this.ddy/1.05, -100, 100);
         }
 
 
@@ -129,8 +129,8 @@ var Ball = function(juego, x, y, gravedad, impulso) {
 
     this.calcula_centro = function (dt) {
 
-        this.center_x = this.x + (this.dx * dt) + this.ancho/2;
-        this.center_y = this.y + (this.dy * dt) + this.alto/2;
+        this.center_x_ = this.x + (this.dx * dt) + this.ancho_/2;
+        this.center_y_ = this.y + (this.dy * dt) + this.alto_/2;
 
     };
 
@@ -140,33 +140,33 @@ var Ball = function(juego, x, y, gravedad, impulso) {
         if(doble){
             mas = Math.PI/4;
         }
-        this.theta += this.dx/10000;
+        this.theta_ += this.dx/10000;
 
         //ROTACION VERTICE
-        var tempX = x - this.center_x;
-        var tempY = y - this.center_y;
+        var tempX = x - this.center_x_;
+        var tempY = y - this.center_y_;
 
-        var rotatedX = tempX*Math.cos(this.theta + mas) - tempY*Math.sin(this.theta + mas);
-        var rotatedY = tempX*Math.sin(this.theta + mas) + tempY*Math.cos(this.theta + mas);
+        var rotatedX = tempX*Math.cos(this.theta_ + mas) - tempY*Math.sin(this.theta_ + mas);
+        var rotatedY = tempX*Math.sin(this.theta_ + mas) + tempY*Math.cos(this.theta_ + mas);
 
         var ret = {};
-        ret.x = rotatedX + this.center_x;
-        ret.y = rotatedY + this.center_y;
+        ret.x = rotatedX + this.center_x_;
+        ret.y = rotatedY + this.center_y_;
         return ret;
 
     };
     this.render = function (ctx, dt) {
         if(this.mate){
-            ctx.fillStyle   = juego.COLOR.SLATE;
+            ctx.fillStyle   = juego.COLOR_.SLATE;
         }
         else{
-            ctx.fillStyle   = juego.COLOR.GOLD;
+            ctx.fillStyle   = juego.COLOR_.GOLD;
         }
 
         var v1 = this.calcula_rotacion(this.x + (this.dx * dt), this.y + (this.dy * dt),false, dt);
-        var v2 = this.calcula_rotacion(this.x + (this.dx * dt) + this.ancho, this.y + (this.dy * dt),false, dt);
-        var v3 = this.calcula_rotacion(this.x + (this.dx * dt) + this.ancho, this.y + (this.dy * dt) + this.alto,false, dt);
-        var v4 = this.calcula_rotacion(this.x + (this.dx * dt), this.y + (this.dy * dt) + this.alto,false, dt);
+        var v2 = this.calcula_rotacion(this.x + (this.dx * dt) + this.ancho_, this.y + (this.dy * dt),false, dt);
+        var v3 = this.calcula_rotacion(this.x + (this.dx * dt) + this.ancho_, this.y + (this.dy * dt) + this.alto_,false, dt);
+        var v4 = this.calcula_rotacion(this.x + (this.dx * dt), this.y + (this.dy * dt) + this.alto_,false, dt);
 
         ctx.beginPath();
         ctx.moveTo(v1.x, v1.y);
