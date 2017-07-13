a46ce86c8cf603180e0298f121d5f22eb4ec5564
jesusolmossanchez <jesusolmossanchez@gmail.com>
2017-07-13 12:21:56 +0200

    Explosion preparado para optimización

--- Explosion.js
+++ Explosion.js
@@ -1,15 +1,15 @@
 /**************************************************
-*** EXPLOSION PLAYER CLASS
+*** EXPLOSION CLASSes
 **************************************************/
 var Explosion = function(x, y, supertiro, punto, ball) {
-    this.particles = [];
-    this.particulas_por_explosion = 15;
+    this.particles_ = [];
+    this.particulas_por_explosion_ = 15;
     if(supertiro){
-        this.particulas_por_explosion = 15 * 3;
+        this.particulas_por_explosion_ = 15 * 3;
     }
 
-    for (var i = 0; i < this.particulas_por_explosion; i++) {
-        this.particles.push(
+    for (var i = 0; i < this.particulas_por_explosion_; i++) {
+        this.particles_.push(
             new Particle(x, y, supertiro, punto, ball)
         );
     }
@@ -19,7 +19,7 @@ var Explosion = function(x, y, supertiro, punto, ball) {
 
 var Particle = function(x, y, supertiro, punto, ball) {
     
-    this.randInt = function(min, max, positive) {
+    this.randInt_ = function(min, max, positive) {
 
         var num;
         if (positive === false) {
@@ -35,35 +35,35 @@ var Particle = function(x, y, supertiro, punto, ball) {
 
 
     //TODO: parametrizar
-    this.particlesMinSpeed      = 3;
-    this.particlesMaxSpeed      = 8;
-    this.particlesMinSize       = 1;
-    this.max_particulas_size       = 8;
+    this.particlesMinSpeed_      = 3;
+    this.particlesMaxSpeed_      = 8;
+    this.particlesMinSize_       = 1;
+    this.max_particulas_size_       = 8;
 
     if(supertiro){
-        this.max_particulas_size = this.max_particulas_size * 1.5;
+        this.max_particulas_size_ = this.max_particulas_size_ * 1.5;
     }
 
     if(punto){
-        this.r    = this.randInt(0, 255);
+        this.r    = this.randInt_(0, 255);
         this.g    = '0';
         this.b    = '0';
     }
     else if(supertiro){
-        this.r    = this.randInt(0, 255);
+        this.r    = this.randInt_(0, 255);
         this.g    = '255';
-        this.b    = this.randInt(0, 255);
+        this.b    = this.randInt_(0, 255);
     }
     else{
         this.r    = '255';
         this.g    = '255';
-        this.b    = this.randInt(0, 255);
+        this.b    = this.randInt_(0, 255);
     }
 
     this.x    = x;
     this.y    = y;
-    this.xv   = this.randInt(this.particlesMinSpeed, this.particlesMaxSpeed, false) + ball.dx/300;
-    this.yv   = this.randInt(this.particlesMinSpeed/2, this.particlesMaxSpeed/2, false) + ball.dy/300;
-    this.size = this.randInt(this.particlesMinSize, this.max_particulas_size, true);
+    this.xv   = this.randInt_(this.particlesMinSpeed_, this.particlesMaxSpeed_, false) + ball.dx/300;
+    this.yv   = this.randInt_(this.particlesMinSpeed_/2, this.particlesMaxSpeed_/2, false) + ball.dy/300;
+    this.size = this.randInt_(this.particlesMinSize_, this.max_particulas_size_, true);
     
 };
